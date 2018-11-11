a1e4ba3763303f3cf84c30c4bd71d8d0a94e9e47
Wieland Hoffmann <themineo@gmail.com>
2018-11-11 19:17:33 +0100

    It works!

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 extern crate failure;
+extern crate geojson;
 #[macro_use]
 extern crate log;
 extern crate env_logger;
 extern crate rexiv2;
+extern crate serde_json;
 extern crate walkdir;
 
+use geojson::{FeatureCollection, Feature, Geometry, Value};
+use serde_json::{Map, to_value};
+use std::convert::From;
+use std::fs;
 use std::path;
 use std::env;
 use walkdir::WalkDir;
@@ -19,26 +25,62 @@ fn get_gps_info(path: &path::Path) -> EMResult<Option<rexiv2::GpsInfo>> {
     }
 }
 
-fn walk_files_in_dir(dirname: &str) {
-    for entry in WalkDir::new(dirname) {
-        let eu = entry.unwrap();
-        let path = eu.path();
-        match get_gps_info(path) {
-            Ok(Some(gpsinfo)) => {
-                info!("{}", path.display());
-                info!("{:?}", gpsinfo);
-            }
-            Ok(None) => info!("No GPS info in {}", path.display()),
-            Err(e) => error!("{}", e),
-        }
-    }
+fn feature_from_gpsinfo(gpsinfo: &rexiv2::GpsInfo, path: &path::Path) -> EMResult<Feature> {
+    info!("{}", path.display());
+    info!("{:?}", gpsinfo);
+    let mut properties = Map::new();
+    properties.insert(String::from("filename"), to_value(path)?);
+    let thispoint = Value::Point(vec![gpsinfo.longitude, gpsinfo.latitude]);
+    let thisgeometry = Geometry::new(thispoint);
+    let thisfeature = Feature {
+        bbox: None,
+        geometry: Some(thisgeometry),
+        id: None,
+        properties: Some(properties),
+        foreign_members: None,
+    };
+    Ok(thisfeature)
 }
 
-fn main() {
+fn featurecollection_from_dir(dirname: &str) -> EMResult<FeatureCollection> {
+    let mut features: Vec<Feature> = vec![];
+    for entry in WalkDir::new(dirname).into_iter().filter(|e| match e {
+        // TODO: This is similar to map_or_else on nightly
+        Ok(f) => f.file_type().is_file(),
+        Err(err) => {
+            error!("{:?}: {}", e, err);
+            false
+        }
+    })
+    {
+        let eu = entry.unwrap();
+        let path = eu.path();
+        let nicepath = path.display();
+        match get_gps_info(path) {
+            Ok(Some(gpsinfo)) => {
+                match feature_from_gpsinfo(&gpsinfo, path) {
+                    Ok(feature) => features.push(feature),
+                    Err(e) => error!("{}: {}", nicepath, e),
+                }
+            }
+            Ok(None) => info!("{}: No GPS info", nicepath),
+            Err(e) => error!("{}: {}", nicepath, e),
+        }
+    }
+    let allfeatures = FeatureCollection {
+        bbox: None,
+        features: features,
+        foreign_members: None,
+    };
+    Ok(allfeatures)
+}
+
+fn main() -> Result<(), failure::Error> {
     env_logger::init();
     let args: Vec<String> = env::args().collect();
-    match args.get(1) {
-        Some(dirname) => walk_files_in_dir(dirname),
-        None => error!("No directory name"),
-    }
+    let indir = args.get(1).expect("No directory name");
+    let outfile = args.get(2).expect("No output file name");
+    featurecollection_from_dir(indir)
+        .and_then(|f| serde_json::to_string(&f).map_err(From::from))
+        .and_then(|s| fs::write(outfile, s).map_err(From::from))
 }
